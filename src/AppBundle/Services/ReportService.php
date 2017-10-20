e00af50b5940ca53ad78999e0c14e75f0df1cf07
Daniel Fredriksen <dfredriksen@nractech.com>
2017-10-20 01:26:16 +0000

    #819 Fixed issue with totals in summary reports

--- src/AppBundle/Services/ReportService.php
+++ src/AppBundle/Services/ReportService.php
@@ -23,10 +23,31 @@ class ReportService
                 }
             }
 
+            $product_visits = [];
+
             foreach($report_data as $record)
             {
                 $user = $record['user_id'];
- 
+                if(!isset($product_visits[$user])) {
+                    $product_visits[$user] = [];
+                }
+
+                $category_parts = explode('_', $record['category']);
+                $product_id = null;
+                $view_index = $category_parts[0];
+                if(!isset($product_visits[$user][$view_index])) {
+                    $product_visits[$user][$view_index] = [];
+                }
+
+                if(strlen($category_parts[1]) == 36)
+                {
+                    $product_id = $category_parts[1];
+                    $products[$product_id] = $product_id;
+                    if(!isset($product_visits[$user][$view_index][$product_id])) {
+                        $product_visits[$user][$view_index][$product_id] = 0;
+                    }
+                }                
+
                 if(empty($summary_data[$user]))
                 {
                     $summary_data[$user] = [
@@ -38,15 +59,16 @@ class ReportService
                        ,'session_id' => $record['session_id']
                        ,'Shopping_Abandoned (0=No, 1=Yes)' => 0
                        ,'total_time_spent_on_website' => 0
-                       ,'total_time_spent_on_instructions_page' => 0
-                       ,'total_visits_to_SRS_page' => 0
-                       ,'total_time_spent_on_SRS_page' => 0
+                       ,'total_time_spent_on_Instructions_pages' => 0
+                       ,'total_visits_to_SRS_pages' => 0
+                       ,'total_time_spent_on_SRS_pages' => 0
                        ,'total_visits_to_PIS_pages' => 0
                        ,'total_time_spent_on_PIS_pages' => 0
                        ,'total_visits_to_CRS_pages' => 0
                        ,'total_time_spent_on_CRS_pages' => 0
-                       ,'total_visits_to_PS_page' => 0
-                       ,'total_time_spent_on_PS_page' => 0
+                       ,'total_visits_to_PS_pages' => 0
+                       ,'total_time_spent_on_PS_pages' => 0
+                       ,'total_visits_to_Instructions_pages' => 0
                     ];
 
                     foreach($products as $product_id)
@@ -89,13 +111,13 @@ class ReportService
 
                     if(stristr($record['category'], 'Instructions_') > -1)
                     {
-                        $time_string = 'total_time_spent_on_instructions_page';
+                        $time_string = 'total_time_spent_on_Instructions_pages';
                     }
 
                     if(stristr($record['category'], 'SRS_') > -1)
                     {
-                        $time_string = 'total_time_spent_on_SRS_page';
-                        $visit_string = 'total_visits_to_SRS_page';
+                        $time_string = 'total_time_spent_on_SRS_pages';
+                        $visit_string = 'total_visits_to_SRS_pages';
                     }
 
                     if(stristr($record['category'], 'PIS_') > -1)
@@ -112,8 +134,8 @@ class ReportService
 
                     if(stristr($record['category'], 'PS_') > -1)
                     {
-                        $time_string = 'total_time_spent_on_PS_page';
-                        $visit_string = 'total_visits_to_PS_page';
+                        $time_string = 'total_time_spent_on_PS_pages';
+                        $visit_string = 'total_visits_to_PS_pages';
                     }
 
                     if(!empty($time_string))
@@ -127,7 +149,7 @@ class ReportService
                     if(!empty($visit_string))
 					{
                        //*push
-                        if($visit_string=='total_visits_to_SRS_page')
+                        if($visit_string=='total_visits_to_SRS_pages')
                         {
                             if($summary_data[$user][$visit_string] < $visit )
                             {
@@ -147,6 +169,10 @@ class ReportService
 
                     if(!empty($visit_string) && !empty($product_id) && $visit > $summary_data[$user]['Product_' . $product_id . '_' . $visit_string])
                         $summary_data[$user]['Product_' . $product_id . '_' . $visit_string] = $visit;
+
+                    if(!empty($product_id) && !empty($view_index) && $visit > $product_visits[$user][$view_index][$product_id]) {
+                            $product_visits[$user][$view_index][$product_id] = $visit;
+                    }
 
                 }
 
@@ -211,7 +237,31 @@ class ReportService
                         $summary_data[$user]["Product_$product_id" . "_Number_of_Times_FilterBy1Star_clicked"]++;
 
                 }
+            }
+        }
 
+        $total_visits = [];
+
+        if(!empty($product_visits)) {
+            foreach($product_visits as $user=>$views) {
+                if(!isset($total_visits[$user])) {
+                    $total_visits[$user] = [];
+                }
+                foreach($views as $view_index=>$products) {
+                    if(!isset($total_visits[$user][$view_index])) {
+                        $total_visits[$user][$view_index] = 0;
+                    }
+
+                    foreach($products as $product_id=>$tally) {
+                        $total_visits[$user][$view_index] += $tally;
+                    }
+                }
+            }
+        }
+
+        foreach($total_visits as $user=>$views) {
+            foreach($views as $view_index=>$total) {
+                $summary_data[$user]['total_visits_to_' . $view_index . '_pages'] = $total;
             }
         }
 
