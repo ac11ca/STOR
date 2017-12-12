c79c70889682c6fdfe3482f5d495195cb0b5451a
Abdullah Kiser <kiser.bd@gmail.com>
2017-12-12 22:36:16 +0600

    Debug the summary report

--- src/AppBundle/Repository/AnalyticsRepository.php
+++ src/AppBundle/Repository/AnalyticsRepository.php
@@ -85,7 +85,7 @@ class AnalyticsRepository extends ApplicationMasterRepository
         $query->addSelect('u');
         $query->addSelect('s');
         $query->addSelect('c');
-        $query->orderBy('u.id');
+        $query->orderBy('s.User.id');
         $query = $this->constructQueryFilter($dimension, $condition, $operator, $value, $query);
         
         $results = $query->getQuery()->getResult();
